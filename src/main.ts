67d30aa887082722fd749815e751a66efa92a878
McNugget <ethan.d.seys@gmail.com>
2024-07-11 19:36:14 -0500

    0.0.2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import type { ATIS } from './types.js';
+import type { ATIS, vATIS } from './types.js';
 
-const link = `https://datis.clowd.io/api/kstl`;
+const facility = 'KATL';
+
+const link = `https://datis.clowd.io/api/${facility}`;
 
 const findNthOccurrenceIndex = (
   str: string,
@@ -17,21 +19,22 @@ const findNthOccurrenceIndex = (
   );
 };
 
-const splitATIS = (atis: ATIS['datis']) => {
+const parseATIS = (atis: ATIS['datis']): vATIS => {
   const notams = atis.split('NOTAMS... ')[1].split(' ...ADVS')[0];
   const airportConditions = atis
     .slice(findNthOccurrenceIndex(atis, '.', 2) + 1)
     .split(atis.includes('NOTAMS') ? 'NOTAMS...' : 'NOTICE TO AIR MISSIONS.')[0]
     .trim();
 
-  atis.charAt(atis.indexOf('INFO') + 5);
-
   return {
-    atis: {
-      atisLetter: atis.charAt(atis.indexOf('INFO') + 5),
-      airportConditions,
-      notams,
-    },
+    facility,
+    preset: 'REAL WORLD',
+    atisLetter: atis.charAt(atis.indexOf('INFO') + 5),
+    atisType: 'combined', // replace with logic if its combined or dep/arr
+    airportConditions,
+    notams,
+    timestamp: '', // getTimeStamp(atis),
+    version: '4.0.0',
   };
 };
 
@@ -41,7 +44,7 @@ const fetchATIS = () => {
       return response.json() as Promise<ATIS[]>;
     })
     .then((data) => {
-      return splitATIS(data[0].datis);
+      return parseATIS(data[0].datis); // data[0].datis is placeholder data until logic is implemented to parse dep/arr atis's
     });
 };
 
