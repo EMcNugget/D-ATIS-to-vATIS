b4a3f206692715d56d9fd6273c59f1f00d2e45e5
McNugget <ethan.d.seys@gmail.com>
2024-07-20 11:20:03 -0500

    Removed dup code

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -1,40 +1,10 @@
 use crate::settings::read_settings;
+use crate::util::{read_json_file, write_json_file};
 use log::{error, info};
 use serde::Serialize;
 use serde_json::{self, Value};
-use std::fs::File;
-use std::io::{BufReader, Write};
 use tauri::AppHandle;
 
-fn read_json_file(file_path: &str) -> Result<Value, String> {
-    let file = File::open(file_path).map_err(|e| {
-        let err_msg = e.to_string();
-        error!("Failed to open file {}: {}", file_path, err_msg);
-        err_msg
-    })?;
-
-    let reader = BufReader::new(file);
-    let json_value = serde_json::from_reader(reader).map_err(|e| {
-        let err_msg = e.to_string();
-        error!("Failed to read file {}: {}", file_path, err_msg);
-        err_msg
-    })?;
-    Ok(json_value)
-}
-
-fn write_json_file(filename: &str, data: &str) -> Result<(), String> {
-    let mut file = File::create(filename).map_err(|e| {
-        let err_msg = e.to_string();
-        error!("Failed to create file {}: {}", filename, err_msg);
-        err_msg
-    })?;
-    file.write_all(data.as_bytes()).map_err(|e| {
-        let err_msg = e.to_string();
-        error!("Failed to write to file {}: {}", filename, err_msg);
-        err_msg
-    })?;
-    Ok(())
-}
 pub struct FindComposite {
     pub profile_index: usize,
     pub composite_index: usize,
