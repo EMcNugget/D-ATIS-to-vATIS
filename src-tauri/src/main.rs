55360bd0e684b928326b0e3f083ce9301a219353
McNugget <ethan.d.seys@gmail.com>
2024-07-20 10:38:10 -0500

    Added logging depdencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,10 +7,20 @@ mod structs;
 
 use app::write_atis;
 use settings::{read_settings, write_settings};
+use tauri_plugin_log::{Target, TargetKind};
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .targets([
+                    Target::new(TargetKind::Stdout),
+                    Target::new(TargetKind::LogDir { file_name: None }),
+                    Target::new(TargetKind::Webview),
+                ])
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![
             write_settings,
             read_settings,
