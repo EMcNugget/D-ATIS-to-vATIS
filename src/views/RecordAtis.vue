b8aeb4366dbd266ff0ebc9a34c7e45a68299a8b4
EMcNugget <ethan.d.seys@gmail.com>
2024-08-08 16:43:05 -0500

    Re added info

--- src/views/RecordAtis.vue
+++ src/views/RecordAtis.vue
@@ -5,5 +5,8 @@ import Layout from "./Layout.vue";
 <template>
   <Layout>
     <h1 class="absolute mt-6 font-semibold text-4xl mb-10">Coming Soon</h1>
+    <h2 class="absolute mt-16 font-semibold text-2xl">
+      This will be able to record your ATIS's for you.
+    </h2>
   </Layout>
 </template>
