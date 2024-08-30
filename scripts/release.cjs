1d8e6d4869bd11e3e7edaedd772aa290c67e92e6
EMcNugget <ethan.d.seys@gmail.com>
2024-08-29 23:06:24 -0500

    Removed dependabot

--- scripts/release.cjs
+++
@@ -1,7 +0,0 @@
-const exec = require("@actions/exec");
-
-exec.exec("pnpm changeset version");
-
-const packageJson = require("../package.json");
-
-exec.exec(`pnpm tauri-version ${packageJson.version} --no-git`);
